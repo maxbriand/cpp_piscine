af49bdb6b53e5b3e8247e13ac61090a5547467c1
Maxime Briand <mbriand@1F1.42angouleme.fr>
2024-09-05 17:38:04 +0200

    cpp 00 ex00 in progress

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,33 @@
 /*   By: mbriand <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 20:30:01 by mbriand           #+#    #+#             */
-/*   Updated: 2024/09/03 20:30:02 by mbriand          ###   ########.fr       */
+/*   Updated: 2024/09/05 17:37:01 by mbriand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed(void): number(0){}
+
+Fixed::~Fixed(void){}
+
+Fixed::Fixed(const Fixed& fixed_point)
+{
+    *this = fixed_point;
+}
+
+Fixed&  Fixed::operator=(const Fixed& fixed_point)
+{    
+    this->number = fixed_point.number;
+    return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+    return(this->number);
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    this->number = raw;
+}
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,24 +6,30 @@
 /*   By: mbriand <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 20:29:58 by mbriand           #+#    #+#             */
-/*   Updated: 2024/09/03 20:36:35 by mbriand          ###   ########.fr       */
+/*   Updated: 2024/09/05 17:19:30 by mbriand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+#include <iostream>
+
 class   Fixed
 {
     public:
-        Fixed();
-        ~Fixed();
+                Fixed(void);
+                Fixed(const Fixed& number);
+                ~Fixed(void);
+        Fixed&  operator=(const Fixed& number);
+
+    public:
         int     getRawBits(void) const;
         void    setRawBits(int const raw);
 
     private:
-        int                 value;
-        static const int    eight;      
+        int                 number;
+        static const int    bits = 8;
 
 };
 
